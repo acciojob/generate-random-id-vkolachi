4bcd579d029aea2b2c4eb6a909b8305b75812238
vkolachi <36785379+vkolachi@users.noreply.github.com>
2023-11-30 10:37:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
-function makeid(l) {
-  // write your code here
+function random() {
+	let randomNo=parseInt(Math.random()*100);
+	if(randomNo<47){
+		randomNo +=47;
+	}
+	return randomNo;
 }
-
+function makeid(n) {
+  let str="";
+	for(i=0;i<n;i++){
+		str +=String.fromCharCode(random());
+	}
+    return str;
+}
 // Do not change the code below.
 const l = prompt("Enter a number.");
 alert(makeid(l));
