0bc4ea0d7ff97ad420c5936ab302a0102e8143e3
vkolachi <36785379+vkolachi@users.noreply.github.com>
2023-11-30 10:55:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
-function random() {
-	let randomNo=parseInt(Math.random()*100);
-	if(randomNo<47){
-		randomNo +=47;
-	}
-	return randomNo;
-}
+// function random() {
+// 	let randomNo=parseInt(Math.random()*100);
+// 	if(randomNo<47  ){
+// 		randomNo +=32;
+// 	}else if(randomNo>57 && randomNo<64){
+// 		randomNo +=32;
+// 	}else if (randomNo>90 && randomNo<99) {
+// 		randomNo -=12;
+// 	}
+// 	return randomNo;
+// }
 function makeid(n) {
-  let str="";
-	for(i=0;i<n;i++){
-		str +=String.fromCharCode(random());
-	}
-    return str;
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let result = '';
+
+  for (let i = 0; i < l; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    result += characters.charAt(randomIndex);
+  }
+
+  return result;
 }
 // Do not change the code below.
 const l = prompt("Enter a number.");
-alert(makeid(l));
+alert(makeid(l)); 
